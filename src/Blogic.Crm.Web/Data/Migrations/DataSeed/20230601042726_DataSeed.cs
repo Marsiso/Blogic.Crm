e075d0439e3038fec24e56ff3cbe4b47596465c9
Marek Olsak <m_olsak@utb.cz>
2023-06-01 07:33:52 +0200

    fixup! Added client data seed service.

---
+++ src/Blogic.Crm.Web/Data/Migrations/DataSeed/20230601042726_DataSeed.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blogic.Crm.Web.Data.Migrations.DataSeed
+{
+    /// <inheritdoc />
+    public partial class DataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "age",
+                schema: "dbo",
+                table: "consultants");
+
+            migrationBuilder.DropColumn(
+                name: "age",
+                schema: "dbo",
+                table: "clients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "security_stamp",
+                schema: "dbo",
+                table: "consultants",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone",
+                schema: "dbo",
+                table: "consultants",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "birth_number",
+                schema: "dbo",
+                table: "consultants",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "date_born",
+                schema: "dbo",
+                table: "consultants",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "security_stamp",
+                schema: "dbo",
+                table: "clients",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone",
+                schema: "dbo",
+                table: "clients",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "birth_number",
+                schema: "dbo",
+                table: "clients",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "date_born",
+                schema: "dbo",
+                table: "clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "date_born",
+                schema: "dbo",
+                table: "consultants");
+
+            migrationBuilder.DropColumn(
+                name: "date_born",
+                schema: "dbo",
+                table: "clients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "security_stamp",
+                schema: "dbo",
+                table: "consultants",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone",
+                schema: "dbo",
+                table: "consultants",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "birth_number",
+                schema: "dbo",
+                table: "consultants",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AddColumn<short>(
+                name: "age",
+                schema: "dbo",
+                table: "consultants",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "security_stamp",
+                schema: "dbo",
+                table: "clients",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone",
+                schema: "dbo",
+                table: "clients",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "birth_number",
+                schema: "dbo",
+                table: "clients",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AddColumn<short>(
+                name: "age",
+                schema: "dbo",
+                table: "clients",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+        }
+    }
+}
